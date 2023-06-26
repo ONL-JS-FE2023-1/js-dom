f34cba22fb45a59a781b18fd729d0d45f25b9318
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-26 20:34:54 +0300

    Form, value

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ console.dir(document);
 
 function getAlert(event) {
     const btn = event.target;
-    btn.style.display = 'none';
+    // btn.style.display = 'none';
+    console.log('Hi from get Alert')
+    btn.removeEventListener('click', getAlert);
 }
 
 function consoleHello(event) {
@@ -15,9 +17,26 @@ function consoleHello(event) {
 
 const buttonElement = document.getElementById('btn');
 const secondButton = document.getElementById('second-btn');
+const form = document.getElementById('name-form');
 
 console.log(buttonElement);
 console.dir(buttonElement);
 
 buttonElement.addEventListener('click', getAlert);
 secondButton.addEventListener('mouseover', consoleHello);
+form.addEventListener('submit', consoleForm);
+
+function consoleForm(event) {
+    event.preventDefault();
+    console.log(event);
+    
+    const form = event.target;
+    console.dir(form[0]);
+    
+    const value = form[0].value;
+    console.log(value);
+}
+
+// buttonElement.addEventListener('click', (event) => {  // <<<---- BAD PRACTICE
+//     console.log('hi from callback');
+// })
