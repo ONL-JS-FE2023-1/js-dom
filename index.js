c2e7e156fa04a4d83ef26c113e521c9ed28d8689
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-26 19:45:32 +0300

    Practice task resolve

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ function getAlert() {
     alert('Hello, user!')
 }
 
+function consoleHello() {
+    console.log('Hello');
+}
+
 const buttonElement = document.getElementById('btn');
+const secondButton = document.getElementById('second-btn');
 
 console.log(buttonElement);
 console.dir(buttonElement);
 
 buttonElement.addEventListener('click', getAlert);
+secondButton.addEventListener('mouseover', consoleHello);
