26823fc71be54f7a42a95e0935afad0998b3ee2a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-27 18:23:36 +0300

    getElementsByClassName, getElementsByTagName

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-// console.log(this);
+const collection = document.getElementsByClassName('paragraph');
 
-// alert(); // window.alert();
-
-console.dir(document);
-
-function getAlert(event) {
-    const btn = event.target;
-    // btn.style.display = 'none';
-    console.log('Hi from get Alert')
-    btn.removeEventListener('click', getAlert);
+for(let p of collection) {
+    p.style.color = 'green';
 }
 
-function consoleHello(event) {
-    console.log('Hello');
+const btns = document.getElementsByTagName('button');
+
+const arr = [...btns];
+
+function hello() {
+    console.log('Hello, user');
 }
 
-const buttonElement = document.getElementById('btn');
-const secondButton = document.getElementById('second-btn');
-const form = document.getElementById('name-form');
+// btns[0]
+// btns[1]
+// btns[2]
 
-console.log(buttonElement);
-console.dir(buttonElement);
-
-buttonElement.addEventListener('click', getAlert);
-secondButton.addEventListener('mouseover', consoleHello);
-form.addEventListener('submit', consoleForm);
-
-function consoleForm(event) {
-    event.preventDefault();
-    const form = event.target;
-    const value = form[0].value;
-    console.log(`Hello, ${value ? value : 'Anonym'}`);
+for (const btn of btns) {
+    btn.addEventListener('click', hello);
 }
-
-// buttonElement.addEventListener('click', (event) => {  // <<<---- BAD PRACTICE
-//     console.log('hi from callback');
-// })
