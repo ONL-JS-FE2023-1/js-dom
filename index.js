03c3578183f58c1a5a25d888632e13961dfd2959
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-27 20:12:57 +0300

    Temperature task resolve

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
-/*
-
-Задвання: Створити конвертер валют, використовуючи форму
-
-Декомпозиція.
-
-1. Створити HTML-сторінку з формою, яка містить два поля вводу:
-перше поле - введення суми в одній валюті,
-друге поле - вибір валюти, в яку конвертуємо
-
-2. Додати у форму кнопку "Конвенртувати"
-
-3. Написати обробник, який буде викликатися при відправці форми
-
-4. У обробнику отримати значення введеної суми та обраної валюти
-
-5. Створити константи у яких зберігається актуальний курс валют
-
-6. Обчислити конвертовану суму: введена сума * курс обміну
-
-7. Вивести результат конвертації на сторінку
-
-*/
-
-
-const CURRENCY = {
-    USD_TO_UAH: 36.6,
-    EUR_TO_UAH: 40.50
-}
-
-const converterForm = document.querySelector('#currencyConverterForm');
+const converterForm = document.querySelector('#temperatureConverterForm');
 
 converterForm.addEventListener('submit', convertHandler);
 
 function convertHandler(event) {
-    event.preventDefault(); // Зупиняємо стандартну поведінку форми (перезавантаження)
+    event.preventDefault(); // Зупиняємо стандартну поведінку форми (перезавантаження сторінки)
 
-    // Отримуємо значення введеної суми та обраної валюти
-    const amount = Number(document.querySelector('#amount').value);
-    const currency = document.querySelector('#currency').value;
+    // Отримуємо значення введеної температури
+    const temperatureInput = Number(document.querySelector('#temperature').value);
 
-    let convertedAmount;
+    // Обчислюємо температуру в градусах Фаренгейта
+    const fahrenheit = (temperatureInput * 9 / 5) + 32;
 
-    // обчислюємо конвертовану суму
-    if(currency === 'USD') {
-        convertedAmount = amount * CURRENCY.USD_TO_UAH
-    } else if (currency === 'EUR') {
-        convertedAmount = amount * CURRENCY.EUR_TO_UAH
-    } else {
-        throw new Error('Something problem....!')
-    }
 
-    // виводимо результат конвертації
-    document.querySelector('#result').innerHTML = `${amount} ${currency} = ${convertedAmount.toFixed(2)} UAH`;
+    // Виводимо результат конвертації
+    document.querySelector('#result').innerHTML = `${temperatureInput} C = ${fahrenheit.toFixed(2)} F`;
 }
