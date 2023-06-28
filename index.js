b46ffae4dcbef323b325d6f86641efcd9ea7c654
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-28 18:43:13 +0300

    composePath !

--- index.js
+++ index.js
@@ -6,28 +6,56 @@
 
 */
 
-const div = document.querySelector('.box');
-const [red, green] = document.querySelectorAll('.btn');
+// const div = document.querySelector('.box');
+// const [red, green] = document.querySelectorAll('.btn');
 
-console.log(div.classList);
+// console.log(div.classList);
 
-red.addEventListener('click', setRedColor);
-green.addEventListener('click', setGrenColor);
+// red.addEventListener('click', setRedColor);
+// green.addEventListener('click', setGrenColor);
 
-function setRedColor(event) {
-    div.classList.toggle('red-little');
+// function setRedColor(event) {
+//     div.classList.toggle('red-little');
+// }
+
+// function setGrenColor(event) {
+//     div.classList.toggle('green-big')
+// }
+
+// const themeToggle = document.querySelector('#themeToggle');
+// // const body = document.querySelector('body');
+// const body = document.body;
+
+// themeToggle.addEventListener('click', toggleTheme);
+
+// function toggleTheme() {
+//     body.classList.toggle('dark-theme');
+// }
+
+
+const event = new Event('click');
+// console.log(event);
+console.log(event.composedPath());
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', eventHandler);
+
+function eventHandler(event) {
+    // console.log(event);
+    console.log(event.composedPath());
 }
 
-function setGrenColor(event) {
-    div.classList.toggle('green-big')
-}
+/*
 
-const themeToggle = document.querySelector('#themeToggle');
-// const body = document.querySelector('body');
-const body = document.body;
+3 фази події
 
-themeToggle.addEventListener('click', toggleTheme);
+1. Фаза зануреня. Подія стається на рівні ОС, ОС передає її браузеру (Window),
+-> document -> body -> ........ -> елемент, на якому сталася подія
 
-function toggleTheme() {
-    body.classList.toggle('dark-theme');
-}
+2. Фаза цілі. Подія досягла елемента. Елемент - це таргет
+
+3. Фаза сплиття. Подія починає спливати у зворотньому напрямку, 
+тобто від елемента до браузера
+
+*/
