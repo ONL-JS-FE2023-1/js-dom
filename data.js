ca19ff917a8827a55d43208a2ea74ece7e3307fc
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-29 20:49:06 +0300

    CreateUserCard (need refactor)

---
+++ data.js
@@ -0,0 +1,50 @@
+const data = [
+    {
+        id: 1,
+        name: 'John',
+        description: 'user 1',
+        profilePicture: 'https://img.uxwing.com/wp-content/themes/uxwing/download/computers-mobile-hardware/computer-user-icon.svg'
+    },
+    {
+        id: 2,
+        name: 'Josh',
+        description: 'user 2',
+        profilePicture: 'https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ'
+    },
+    {
+        id: 3,
+        name: 'Jane',
+        description: 'user 3',
+        profilePicture: 'https://img.uxwing.com/wp-content/themes/uxwing/download/computers-mobile-hardware/computer-user-icon.svg'
+    },
+    {
+        id: 4,
+        name: 'Iness',
+        description: 'user 4',
+        profilePicture: 'https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ'
+    },
+    {
+        id: 5,
+        name: 'Alex',
+        description: 'user 5',
+        profilePicture: 'https://img.uxwing.com/wp-content/themes/uxwing/download/computers-mobile-hardware/computer-user-icon.svg'
+    },
+    {
+        id: 6,
+        name: 'Nathan',
+        description: 'user 6',
+        profilePicture: 'https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ'
+    },
+    {
+        id: 7,
+        name: 'Tracy',
+        description: 'user 7',
+        profilePicture: 'https://img.uxwing.com/wp-content/themes/uxwing/download/computers-mobile-hardware/computer-user-icon.svg'
+    },
+    {
+        id: 8,
+        name: 'James',
+        description: 'user 8',
+        profilePicture: 'https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ'
+    }
+]
--- index.js
+++ index.js
@@ -1,15 +1,91 @@
-// 1. Створити новий елемент
-const div = document.createElement('div');
+// const telephone = {
+//     name: 'Samsung',
+//     avatar: './',
+//     description: 'BlaBlaBla',
+//     price: '100$'
+// }
 
-// 2. Приєднати новий елемент до будь-якого існуючого на сторінці батьківського елементу
-const body = document.body;
-body.append(div);
-div.append('Hello DOM');
+// console.log(`${telephone.name} коштує ${telephone.price}`);
 
-// Задача: Створити картинку і вставити її в div
 
-const img = document.createElement('img');
-img.setAttribute('src', './images/golden-retriever-puppy.jpg');
-img.setAttribute('alt', 'Dog');
-img.classList.add('picture');
-div.append(img);
+// const telephoneArray = [
+//     {
+//         name: 'Samsung',
+//         avatar: './',
+//         description: 'BlaBlaBla',
+//         price: '100$'
+//     },
+//     {
+//         name: 'Nokia',
+//         avatar: './',
+//         description: 'BlaBlaBla',
+//         price: '50$'
+//     },
+//     {
+//         name: 'Huawei',
+//         avatar: './',
+//         description: 'BlaBlaBla',
+//         price: '200$'
+//     },
+//     {
+//         name: 'Iphone',
+//         avatar: './',
+//         description: 'BlaBlaBla',
+//         price: '100500$'
+//     }
+// ]
+
+// for(const tel of telephoneArray) {
+//     console.log(`${tel.name} коштує ${tel.price}`);
+// }
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+
+const root = document.querySelector('#root');
+
+function createCard(user) {
+    // 1 дія: створення article
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper');
+
+    // 2 дія: створення картинки
+    const img = document.createElement('img');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('card-image');
+
+    // 3 дія: створення h2
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+    h2.classList.add('username');
+
+    // 4 дія: створення p
+    const p = document.createElement('p');
+    p.append(user.description);
+    p.classList.add('description');
+
+    // 5 дія: чіпляємо до article img, h2, p
+    article.append(img, h2, p);
+
+    // 6 дія: повертаємо створений article
+    return article;
+}
+
+const cardArray = data.map((user) => createCard(user));
+
+root.append(...cardArray);
+
+/*
+
+<article class="card-wrapper">
+            <img src="https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ"
+                alt="John" class="card-image">
+            <h2 class="username">John</h2>
+            <p class="description">user 1</p>
+</article>
+
+*/
