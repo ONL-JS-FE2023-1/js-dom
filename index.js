bf2e37ed9f17987e0f33a0f6305d4629d2faa881
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-30 18:30:24 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,78 +1,25 @@
-// const telephone = {
-//     name: 'Samsung',
-//     avatar: './',
-//     description: 'BlaBlaBla',
-//     price: '100$'
-// }
-
-// console.log(`${telephone.name} коштує ${telephone.price}`);
-
-
-// const telephoneArray = [
-//     {
-//         name: 'Samsung',
-//         avatar: './',
-//         description: 'BlaBlaBla',
-//         price: '100$'
-//     },
-//     {
-//         name: 'Nokia',
-//         avatar: './',
-//         description: 'BlaBlaBla',
-//         price: '50$'
-//     },
-//     {
-//         name: 'Huawei',
-//         avatar: './',
-//         description: 'BlaBlaBla',
-//         price: '200$'
-//     },
-//     {
-//         name: 'Iphone',
-//         avatar: './',
-//         description: 'BlaBlaBla',
-//         price: '100500$'
-//     }
-// ]
-
-// for(const tel of telephoneArray) {
-//     console.log(`${tel.name} коштує ${tel.price}`);
-// }
-
-
-
-
-
-/////////////////////////////////////////////////////////////////
-
 const root = document.querySelector('#root');
 
-function createCard(user) {
-    // 1 дія: створення article
-    const article = document.createElement('article');
-    article.classList.add('card-wrapper');
+// 1. Рефактор коду
+// 2. Створення placeholde'ra для картинки
+// 3. Поки наша картинка вантажиться, ми показуємо placeholder, коли картинка завантажиться
+// -> змінюємо наш placeholder на завантажену картинку користувача
 
-    // 2 дія: створення картинки
+function createCard(user) {
+    // 1 дія: створення картинки
     const img = document.createElement('img');
     img.setAttribute('src', user.profilePicture);
     img.setAttribute('alt', user.name);
     img.classList.add('card-image');
 
-    // 3 дія: створення h2
-    const h2 = document.createElement('h2');
-    h2.append(user.name);
-    h2.classList.add('username');
+    // 2 дія: створення h2
+    const h2 = createElement('h2', {classNames: ['username']}, user.name);
 
-    // 4 дія: створення p
-    const p = document.createElement('p');
-    p.append(user.description);
-    p.classList.add('description');
+    // 3 дія: створення p
+    const p = createElement('p', {classNames: ['description']}, user.description);
 
-    // 5 дія: чіпляємо до article img, h2, p
-    article.append(img, h2, p);
-
-    // 6 дія: повертаємо створений article
-    return article;
+    // 4 дія: створення article, повертаємо створений article, чіпляємо до article img, h2, p
+    return createElement('article', {classNames: ['card-wrapper']}, img, h2, p);
 }
 
 const cardArray = data.map((user) => createCard(user));
@@ -89,3 +36,20 @@ root.append(...cardArray);
 </article>
 
 */
+
+/**
+ * 
+ * @param {String} tagName - тег елемента, який потрібно створити
+ * @param {Object} options
+ * @param {String[]} optins.classNames - список класів 
+ * @param  {Node} children - список дочірніх вузлів
+ * @returns {HTMLElement} - створений елемент
+ */
+
+function createElement(tagName, {classNames}, ...children) {
+    const elem = document.createElement(tagName);
+    elem.classList.add(...classNames);
+    elem.append(...children);
+
+    return elem;
+}
