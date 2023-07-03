03d0d3c77ca2c5d31113e453fc142b2585199cb3
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-03 18:20:26 +0300

    TODO-list basic lvl

--- index.js
+++ index.js
@@ -1,83 +1,43 @@
-// +1. Рефактор коду
-// +2. Створення placeholde'ra для картинки
-// +3. Поки наша картинка вантажиться, ми показуємо placeholder, коли картинка завантажиться
-// -> змінюємо наш placeholder на завантажену картинку користувача
-
+const form = document.querySelector('#form');
 const root = document.querySelector('#root');
 
-function imageLoadHandler({ target }) {
-    console.log('image successfully loaded');
-    const parentWrapper = document.getElementById(`wrapper${target.dataset.id}`);
-    parentWrapper.append(target);
+form.addEventListener('submit', addTask);
+
+function addTask(event) {
+    console.log(event);
+    event.preventDefault();
+
+    // +1. Отримати текст завдання, яке користувач ввів до інпуту
+    // console.log(event.target[0].value);
+    const {target, target: {textInput: {value}}} = event;
+
+    // +2. Створити лішку із вказаним текстом завдання і заапендити лішку до списку
+    const li = createElement('li', {classNames: ['item']}, value);
+    root.append(li);
+
+    target.reset();
 }
 
-function imageErrorHandler({ target }) {
-    target.remove();
-    console.log('image loading has error');
-}
 
-function createCard(user) {
-    // 1 дія: створення обгортки для картинки
-    const imageWrapper = createImageWrapper(user);
 
-    // 3 дія: створення h2
-    const h2 = createElement('h2', { classNames: ['username'] }, user.name);
 
-    // 4 дія: створення p
-    const p = createElement('p', { classNames: ['description'] }, user.description);
 
-    // 5 дія: створення article, повертаємо створений article, чіпляємо до article img, h2, p
-    return createElement('article', { classNames: ['card-wrapper'] }, imageWrapper, h2, p);
-}
 
-/**
- * 
- * @param {Object} user - об'єкт юзера, картинку для якого створюємо
- * @returns {HTMLElement} - картинка, яку ми створили
- */
-function createUserImage(user) {
-    // 1 дія: створюємо картинку
-    const img = document.createElement('img');
 
-    // 2 дія: додаємо атрибути і класи до картинки
-    img.setAttribute('src', user.profilePicture);
-    img.setAttribute('alt', user.name);
-    img.dataset.id = user.id; // data-id
-    img.classList.add('card-image');
 
-    // 3 дія: реєструємо обробники подій завантаження ресурсу
-    img.addEventListener('load', imageLoadHandler);
-    img.addEventListener('error', imageErrorHandler);
 
-    return img;
-}
 
-function createImageWrapper(user) {
-    // 1 дія: створення обгортки для картинки. Обгортка - placeholder
-    const imgWrapper = createElement('div', { classNames: ['image-wrapper'] });
-    imgWrapper.setAttribute('id', `wrapper${user.id}`);
-    imgWrapper.style.backgroundColor = stringToColor(user.name);
-    // 2 дія: створення картинки
-    const img = createUserImage(user);
-    return imgWrapper;
-}
 
-const cardArray = data.map((user) => createCard(user));
 
-root.append(...cardArray);
 
-/*
 
-<article class="card-wrapper">
-            <div class="image-wrapper">
-                <img src="https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ"
-                    alt="John" class="card-image">
-            </div>
-            <h2 class="username">John</h2>
-            <p class="description">user 1</p>
-        </article>
 
-*/
+
+
+
+
+
+
 
 /**
  * 
@@ -87,7 +47,6 @@ root.append(...cardArray);
  * @param  {Node} children - список дочірніх вузлів
  * @returns {HTMLElement} - створений елемент
  */
-
 function createElement(tagName, { classNames }, ...children) {
     const elem = document.createElement(tagName);
     elem.classList.add(...classNames);
@@ -95,20 +54,3 @@ function createElement(tagName, { classNames }, ...children) {
 
     return elem;
 }
-
-function stringToColor(str) {
-    let hash = 0;
-    str.split('').forEach(char => {
-        hash = char.charCodeAt(0) + ((hash << 5) - hash)
-    })
-    let colour = '#'
-    for (let i = 0; i < 3; i++) {
-        const value = (hash >> (i * 8)) & 0xff
-        colour += value.toString(16).padStart(2, '0')
-    }
-    return colour
-}
-
-// Що можна покращити?
-// Ініціали користувача в середині обгортки, якщо картинки ще немає
-// Потрібно створити div, який буде вам показувати ініціали, якщо картинки немає
