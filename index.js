85d4fde02f3d25b0128b76ce323f93bfb8c5ee19
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-03 19:30:14 +0300

    TODO-list (edit **)

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ function addTask(event) {
     deleteBtn.addEventListener('click', deleteHandler);
     li.append(deleteBtn);
 
+    // 4. Створити кнопку для редагування саме цієї лішки
+    const editBtn = createElement('button', {classNames: ['editBtn']}, 'Edit');
+    editBtn.addEventListener('click', editHandler);
+    li.append(editBtn);
+
     target.reset();
 }
 
@@ -29,6 +34,31 @@ function deleteHandler({target: {parentNode}}) {
     // 2. Видалити батьківську лішку
     // Примітка. Батьківська лішка видалиться разом з кнопкою видалення
     parentNode.remove();
+}
+
+function editHandler({target, target: {parentNode}}) {
+    const editInput = createElement('input', {classNames: []});
+    editInput.setAttribute('placeholder', 'enter new text');
+    target.removeEventListener('click', editHandler); // видаляємо старий обробник, щоб повісити новий
+    target.textContent = 'Save'; // перейменовуємо кнопку edit => save
+    target.addEventListener('click', saveHandler); // реєструємо новий обробник
+    parentNode.append(editInput);
+    editInput.focus();
+}
+
+function saveHandler({target, target: {parentNode, nextSibling}}) {
+    // оператор опціональної послідовності, задля уникнення помилки
+    // при перемиканні режимів (повторному) - сіблінгів не буде
+
+
+    // 1. Зберегти значення з інпуту редагування
+    const editValue = nextSibling?.value;
+    // прокидуємо значення з інпута редагування лішці
+    parentNode.childNodes[0].nodeValue = editValue;
+
+    target.textContent = 'Edit';
+    target.nextSibling?.remove(); // видаляємо інпут редагування
+    target.addEventListener('click', editHandler);
 }
 
 
