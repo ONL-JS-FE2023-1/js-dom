28d469d514b86ddec6a0276bb511d78f36f2f22b
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-03 20:48:44 +0300

    RegExp

--- index.js
+++ index.js
@@ -1,99 +1,17 @@
-const form = document.querySelector('#form');
-const root = document.querySelector('#root');
+// const regexp = /^Test$/;
+///^[A-Za-z]{2,5}$/ // шукаємо слово з 2-5 (великих і маленьких)
 
-form.addEventListener('submit', addTask);
+// sunnyday@gmail.com
+//   1       2     3
 
-function addTask(event) {
-    event.preventDefault();
+// /^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$/
 
-    // +1. Отримати текст завдання, яке користувач ввів до інпуту
-    // console.log(event.target[0].value);
-    const {target, target: {textInput: {value}}} = event;
+const testSTring = 'sunnyday23@gmail.com';
 
-    // +2. Створити лішку із вказаним текстом завдання і заапендити лішку до списку
-    const li = createElement('li', {classNames: ['item']}, value);
-    root.append(li);
+// Літеральне створення регулярних виразів
+const reg1 = /^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$/;
+console.log(reg1);
 
-    // 3. Створити кнопку для видалення саме цієї лішки
-    const deleteBtn = createElement('button', {classNames: ['deleteBtn']}, 'X');
-    deleteBtn.addEventListener('click', deleteHandler);
-    li.append(deleteBtn);
-
-    // 4. Створити кнопку для редагування саме цієї лішки
-    const editBtn = createElement('button', {classNames: ['editBtn']}, 'Edit');
-    editBtn.addEventListener('click', editHandler);
-    li.append(editBtn);
-
-    target.reset();
-}
-
-function deleteHandler({target: {parentNode}}) {
-    // 1. Отримати доступ до батьківської лішки
-    // console.log(event.target.parentNode);
-
-    // 2. Видалити батьківську лішку
-    // Примітка. Батьківська лішка видалиться разом з кнопкою видалення
-    parentNode.remove();
-}
-
-function editHandler({target, target: {parentNode}}) {
-    const editInput = createElement('input', {classNames: []});
-    editInput.setAttribute('placeholder', 'enter new text');
-    target.removeEventListener('click', editHandler); // видаляємо старий обробник, щоб повісити новий
-    target.textContent = 'Save'; // перейменовуємо кнопку edit => save
-    target.addEventListener('click', saveHandler); // реєструємо новий обробник
-    parentNode.append(editInput);
-    editInput.focus();
-}
-
-function saveHandler({target, target: {parentNode, nextSibling}}) {
-    // оператор опціональної послідовності, задля уникнення помилки
-    // при перемиканні режимів (повторному) - сіблінгів не буде
-
-
-    // 1. Зберегти значення з інпуту редагування
-    const editValue = nextSibling?.value;
-    // прокидуємо значення з інпута редагування лішці
-    parentNode.childNodes[0].nodeValue = editValue;
-
-    target.textContent = 'Edit';
-    target.nextSibling?.remove(); // видаляємо інпут редагування
-    target.addEventListener('click', editHandler);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * 
- * @param {String} tagName - тег елемента, який потрібно створити
- * @param {Object} options
- * @param {String[]} optins.classNames - список класів 
- * @param  {Node} children - список дочірніх вузлів
- * @returns {HTMLElement} - створений елемент
- */
-function createElement(tagName, { classNames }, ...children) {
-    const elem = document.createElement(tagName);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-
-    return elem;
-}
+// Створенння регулярних виразів за допомогою конструктора
+const reg2 = new RegExp('^[A-Za-z]{2,5}$');
+console.log(reg2);
