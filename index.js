f46b95281632a99ffbfc047be06506a4b50a18af
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-04 20:53:09 +0300

    JSON welcome

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const timeoutID = setTimeout(func, 1000);
-clearTimeout(timeoutID);
-
-const intervalID = setInterval(function () {console.log('LOG 5')}, 2000);
-clearInterval(intervalID);
-
-function func() {
-    console.log(2 + 2);
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    avatar: '',
+    age: 65,
+    footSize: 46,
+    address: {
+        street: 'Honcharenko',
+        house: 64
+    },
+    sayHello: function() { // function will be ignored
+        console.log('Hello!')
+    }
+    // крім цього, будуть ігноруватись Symbol() та undefined
 }
-
-// func(); // 4
-
-// let j = 1;
-// for(let i = 0; i < 1000000000; i++) {
-//     j *= 5;
-// }
