48624059f889f2a733f558145fa9e865ea7b4c0e
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-05 18:18:31 +0300

    Home task resolve

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ const user = {
     }
     // крім цього, будуть ігноруватись Symbol() та undefined
 }
+
+const helloTimeoutId = setTimeout(function() {alert('Hello user!')}, 2000);
+clearTimeout(helloTimeoutId); // task 1
+
+
+// task 2
+
+const myObject = {
+    numberValue: 123,
+    stringValue: 'Hello',
+    nullValue: null,
+    objectValue: { nestedProperty: 'Nested' },
+    functionValue: () => { // при перетворенні в JSON, функція буде проігнорована
+        console.log('Hello from function')
+    },
+    undefinedValue: undefined, // undefined буде проігнорований
+    symbolValue: Symbol('mySymbol'), // symbol буде проігнорований
+    arrayValue: [1, 2, 3]
+}
+
+// Перетворення об'єкта на JSON
+const jsonString = JSON.stringify(myObject);
+
+// Перетворення JSON на об'єкт JS
+const parsedObject = JSON.parse(jsonString);
