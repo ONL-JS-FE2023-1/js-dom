e481051f7b5ff0fee4d095fe2725905b3ade8482
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-05 20:45:55 +0300

    Creating Promise

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const p = fetch('./john.json')
-.then((response) => {
-    return response.json();
-}).then((data) => {
-    console.log(data);
-}).catch((err) => {
-    console.log(err);
-}).finally(() => {
-    console.log('hi from finally')
-})
+const p = new Promise(executor);
+
+function executor(resolve, reject) {
+    // задача:
+    // згенерувати випадкове ціле число в діапазоеі від 0 до 15
+    const number = Math.floor(Math.random() * 15);
+
+    // якщо число кратне 2 -> резолвимо проміс та повертаємо це значення
+    // якщо число не кратне 2 -> реджектимо проміс та повертаємо помилку
+
+    if(number % 2 === 0) {
+        resolve(number);
+    } else {
+        const err = new RangeError(`Error happenned: ${number}!`);
+        reject(err);
+        // reject(new RangeError('Error happenned!'));
+    }
+}
+
+p.then((data) => {console.log(data)}, (err) => {console.log(err)})
