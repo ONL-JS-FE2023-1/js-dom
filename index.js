a6869f774402ce46968a2d90fed9bb1e0f3b3e70
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-05 18:41:19 +0300

    Deep copy

--- index.js
+++ index.js
@@ -1,40 +1,13 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    avatar: '',
-    age: 65,
-    footSize: 46,
-    address: {
-        street: 'Honcharenko',
-        house: 64
-    },
-    sayHello: function() { // function will be ignored
-        console.log('Hello!')
-    }
-    // крім цього, будуть ігноруватись Symbol() та undefined
+// Копії бувають: глибокі та поверхневі
+
+const originalObject = {
+    users: [
+        {
+            name: 'John',
+            age: 45
+        }
+    ]
 }
 
-const helloTimeoutId = setTimeout(function() {alert('Hello user!')}, 2000);
-clearTimeout(helloTimeoutId); // task 1
-
-
-// task 2
-
-const myObject = {
-    numberValue: 123,
-    stringValue: 'Hello',
-    nullValue: null,
-    objectValue: { nestedProperty: 'Nested' },
-    functionValue: () => { // при перетворенні в JSON, функція буде проігнорована
-        console.log('Hello from function')
-    },
-    undefinedValue: undefined, // undefined буде проігнорований
-    symbolValue: Symbol('mySymbol'), // symbol буде проігнорований
-    arrayValue: [1, 2, 3]
-}
-
-// Перетворення об'єкта на JSON
-const jsonString = JSON.stringify(myObject);
-
-// Перетворення JSON на об'єкт JS
-const parsedObject = JSON.parse(jsonString);
+// const shallowCopy = { ...originalObject }
+const deepCopy = JSON.parse(JSON.stringify(originalObject));
