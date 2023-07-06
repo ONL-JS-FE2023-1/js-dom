b4ccc454f95be3038b63b7cdb9599c298db0417f
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-06 20:01:11 +0300

    Promisify loading image

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const p = new Promise(function(resolve, reject) { // status: pending
-    setTimeout(() => {
-        resolve('It time');
-    }, 15000); // status: fulfilled
+// https://klike.net/uploads/posts/2020-06/1591254385_5.jpg
+
+function loadImage(src) {
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
+    img.setAttribute('alt', 'Picture');
+
+    return new Promise(function(resolve, reject) { // status: pending
+        img.addEventListener('load', () => {
+            clearTimeout(timeoutId)
+            resolve(img); // status: fulfilled
+        })
+        img.addEventListener('error', () => {
+            reject(new Error('Image cannot be loaded')); // status: rejected
+        })
+
+        const timeoutId = setTimeout(() => {
+            reject(new Error('Час сплив!'))
+        }, 40000);
+    })
+}
+
+loadImage('https://klike.net/uploads/posts/2020-06/1591254385_5.jpg')
+.then((img) => {
+    document.body.append(img);
+})
+.catch((err) => {
+    console.error(err);
 })
