e2abf2d1a7168fd6c885721311c3103d30c6b7ae
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-06 19:20:02 +0300

    Validation data with promises

--- index.js
+++ index.js
@@ -1,20 +1,78 @@
-const p = new Promise(executor);
+// Задача: є невеличка форма і нам потрібно її валідувати
 
-function executor(resolve, reject) {
-    // задача:
-    // згенерувати випадкове ціле число в діапазоеі від 0 до 15
-    const number = Math.floor(Math.random() * 15);
+/*
 
-    // якщо число кратне 2 -> резолвимо проміс та повертаємо це значення
-    // якщо число не кратне 2 -> реджектимо проміс та повертаємо помилку
+Використання промісу при валідації даних:
+- Створити функцію, яка приймає дані для валідації
+- Створити новий проміс, який виконує перевірку валідності даних
+- Якщо дані валідні - резолвимо проміс
+- Якщо дані невалідні - реджектимо проміс
 
-    if(number % 2 === 0) {
-        resolve(number);
-    } else {
-        const err = new RangeError(`Error happenned: ${number}!`);
-        reject(err);
-        // reject(new RangeError('Error happenned!'));
-    }
+*/
+
+
+function validateData(data) {
+    return new Promise((resolve, reject) => {
+        // регулярка скопійована з ihateregex
+        const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+
+        if(!data.name || data.name.trim().length === 0) {
+            reject(new Error('Please enter a valid name!'));
+        } else if(!data.email || !emailRegex.test(data.email)) {
+            reject(new Error('Please enter a valid email!'));
+        } else {
+            resolve('Data is valid');
+        }
+    })
 }
 
-p.then((data) => {console.log(data)}, (err) => {console.log(err)})
+const submitBtn = document.querySelector('#submitBtn');
+
+submitBtn.addEventListener('click', submitHandler);
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    const nameInput = document.querySelector('#nameInput');
+    const emailInput = document.querySelector('#emailInput');
+
+    // Робимо div з повідомленням про помилку (на випадок помилки)
+    const errorDiv = document.createElement('div');
+    errorDiv.style.backgroundColor = 'red';
+    errorDiv.style.color = 'white';
+    errorDiv.setAttribute('id', 'errorDiv');
+
+    // Створюємо об'єкт із даними, які ввів користувач
+    const formData = {
+        name: nameInput.value.trim(),
+        email: emailInput.value.trim()
+    }
+
+    // Валідуємо дані
+    validateData(formData)
+    .then((message) => {
+        console.log('Validation successfull: ', message);
+        
+        // Якщо в нас є div з повідомленням про помилку, то ми видаляємо його
+        if(document.querySelector('#errorDiv') !== null) {
+            document.querySelector('#errorDiv').remove();
+        }
+
+        // document.querySelector('#errorDiv')?.remove(); // варіант без if
+
+        // на цьому моменті ми можемо зробити запит на сервер і передати туди дані, які ввів користувач
+
+        document.querySelector('#myForm').reset();
+    }, (error) => {
+        console.error('Validation error: ', error.message);
+        
+        errorDiv.textContent = error.message;
+        // Перевіряємо, чи є на сторінці елемент з id errorDiv
+        // Якщо div з помилкою є - видаляємо його з верстки
+        // Видалення з верстки нам потрібно для того, щоб оновити повідомлення про помилку
+        if(document.querySelector('#errorDiv') !== null) {
+            document.querySelector('#errorDiv').remove();
+        }
+        document.querySelector('#myForm').append(errorDiv);
+    })
+}
