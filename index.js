57fcc53f4ce6d7f00ca59a85b0559a985e188c80
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-06 20:47:02 +0300

    Weather widget (2/3)

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-// https://klike.net/uploads/posts/2020-06/1591254385_5.jpg
+// https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=f7c576ba3699bdd0b98ddcf196639992&units=metric
 
-function loadImage(src) {
-    const img = document.createElement('img');
-    img.setAttribute('src', src);
-    img.setAttribute('alt', 'Picture');
+/*
 
-    return new Promise(function(resolve, reject) { // status: pending
-        img.addEventListener('load', () => {
-            clearTimeout(timeoutId)
-            resolve(img); // status: fulfilled
-        })
-        img.addEventListener('error', () => {
-            reject(new Error('Image cannot be loaded')); // status: rejected
-        })
+Задача: зробити погодний віджет
 
-        const timeoutId = setTimeout(() => {
-            reject(new Error('Час сплив!'))
-        }, 40000);
+Алгоритм вирішення задачі:
+
++ 1. Зробити верстку елементів, які отримують від користувача дані про місто
++ 2. Отримати дані з апі і обробити їх (підготувати дані до запиту на сервер)
+3. Зробити картки з погодою та відобразити їх
+
+*/
+
+const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
+const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', buttonClickHandler);
+
+function buttonClickHandler({target}) {
+    const selectValue = target.previousElementSibling.value;
+    requestAPI(selectValue);
+}
+
+function requestAPI(city) {
+    // робимо тут запит
+    // готуємо URL
+    const url = `${API_BASE}?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        displayWeather(data);
     })
 }
 
-loadImage('https://klike.net/uploads/posts/2020-06/1591254385_5.jpg')
-.then((img) => {
-    document.body.append(img);
-})
-.catch((err) => {
-    console.error(err);
-})
+function displayWeather(weatherData) {
+    // 1. Ми повинні витягнути з того великого об'єкта необхідні нам дані
+    // 2. Динамічно створюємо картку з погодою
+    // 3. Додаємо цю створену картку з погодою до секції з класом wrapper
+    console.log(weatherData)
+}
+
+
+/*
+
+<!-- <article class="weather">
+        <p>City: </p>
+        <p>Temperature: </p>
+        <p>Pressure: </p>
+        <p>Weather description: </p>
+        <p>Wind: </p>
+    </article> -->
+
+*/
