1828c90c0cce5b545c04dd2acc618e669a3fd848
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-07 18:57:07 +0300

    Weather widget (refactor)

--- index.js
+++ index.js
@@ -1,93 +1,39 @@
-// https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=f7c576ba3699bdd0b98ddcf196639992&units=metric
-
-/*
-
-Задача: зробити погодний віджет
-
-Алгоритм вирішення задачі:
-
-+ 1. Зробити верстку елементів, які отримують від користувача дані про місто
-+ 2. Отримати дані з апі і обробити їх (підготувати дані до запиту на сервер)
-+ 3. Зробити картки з погодою та відобразити їх
-
-*/
-
 const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
 const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
-const CITIES = [];
 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', buttonClickHandler);
 
-function buttonClickHandler({target}) {
+function buttonClickHandler({ target }) {
     const selectValue = target.previousElementSibling.value;
-
-    // запобігання створенню карток для міста, для якого ми вже зробили картку
-    if(CITIES.includes(selectValue) === false) {
-        CITIES.push(selectValue);
-        requestAPI(selectValue);
-    }
-    if(CITIES.includes(selectValue) === true) {
-        const searchCity = document.querySelector(`#${selectValue}`);
-        searchCity.remove(); // need review
-        requestAPI(selectValue);
-    }
+    requestAPI(selectValue);
 }
 
 function requestAPI(city) {
-    // робимо тут запит
-    // готуємо URL
     const url = `${API_BASE}?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        displayWeather(data);
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            displayWeather(data);
+        })
 }
 
-/*
-
-<!-- <article class="weather">
-        <p>City: </p>
-        <p>Temperature: </p>
-        <p>Pressure: </p>
-        <p>Weather description: </p>
-        <p>Wind: </p>
-    </article> -->
-
-*/
-
 function displayWeather(weatherData) {
-    // 1. Ми повинні витягнути з того великого об'єкта необхідні нам дані
-    // 2. Динамічно створюємо картку з погодою
-    // 3. Додаємо цю створену картку з погодою до секції з класом wrapper
+    const { name, main: { temp, pressure }, weather: [{ description }], wind: { speed } } = weatherData; // weather need attention!
 
-    const {name, main: {temp, pressure}, weather: [{description}], wind: {speed}} = weatherData; // weather need attention!
-    
-    const article = document.createElement('article');
-    article.classList.add('weather');
-    article.setAttribute('id', name);
+    const article = document.querySelector('#weather-box');
+    article.style.display = 'unset';
 
-    const city = document.createElement('p');
-    city.append('City: ', name);
+    appendInfo('city', name);
+    appendInfo('temp', temp);
+    appendInfo('press', pressure);
+    appendInfo('descr', description);
+    appendInfo('wind', speed);
+}
 
-    const temperature = document.createElement('p');
-    temperature.append('Temperature: ', temp);
-
-    const press = document.createElement('p');
-    press.append('Pressure: ', pressure);
-
-    const descr = document.createElement('p');
-    descr.append('Weather description: ', description);
-
-    const wind = document.createElement('p');
-    wind.append('Wind speed: ', speed);
-
-    article.append(city, temperature, press, descr, wind);
-
-
-    const section = document.querySelector('.wrapper');
-    section.append(article);
+function appendInfo(id, info) {
+    const element = document.querySelector(`#${id}`);
+    element.textContent = info;
 }
