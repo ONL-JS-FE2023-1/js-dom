fd3d6d2d9cf996e0a461917c504b0af0383b67f9
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-07 19:33:40 +0300

    Load data from JSON

--- index.js
+++ index.js
@@ -1,44 +1,85 @@
-const root = document.querySelector('#root');
-
-// 1. Рефактор коду
-// 2. Створення placeholde'ra для картинки
-// 3. Поки наша картинка вантажиться, ми показуємо placeholder, коли картинка завантажиться
+// +1. Рефактор коду
+// +2. Створення placeholde'ra для картинки
+// +3. Поки наша картинка вантажиться, ми показуємо placeholder, коли картинка завантажиться
 // -> змінюємо наш placeholder на завантажену картинку користувача
 
-function createCard(user) {
-    // 1 дія: створення картинки
-    const img = document.createElement('img');
-    img.setAttribute('src', user.profilePicture);
-    img.setAttribute('alt', user.name);
-    img.classList.add('card-image');
+const root = document.querySelector('#root');
 
-    // 2 дія: створення h2
-    const h2 = createElement('h2', {classNames: ['username']}, user.name);
-
-    // 3 дія: створення p
-    const p = createElement('p', {classNames: ['description']}, user.description);
-
-    // 4 дія: створення article, повертаємо створений article, чіпляємо до article img, h2, p
-    return createElement('article', {classNames: ['card-wrapper']}, img, h2, p);
+function imageLoadHandler({ target }) {
+    console.log('image successfully loaded');
+    const parentWrapper = document.getElementById(`wrapper${target.dataset.id}`);
+    parentWrapper.append(target);
 }
 
+function imageErrorHandler({ target }) {
+    target.remove();
+    console.log('image loading has error');
+}
 
-// Задача: у вас немає оцього об'єкту task
-// Але у вас є data.json
-// Зробити запит до data.json -> відмалювати картки для юзерів
+function createCard(user) {
+    // 1 дія: створення обгортки для картинки
+    const imageWrapper = createImageWrapper(user);
 
-const cardArray = data.map((user) => createCard(user)); // 1
+    // 3 дія: створення h2
+    const h2 = createElement('h2', { classNames: ['username'] }, user.name);
 
-root.append(...cardArray); // 2
+    // 4 дія: створення p
+    const p = createElement('p', { classNames: ['description'] }, user.description);
+
+    // 5 дія: створення article, повертаємо створений article, чіпляємо до article img, h2, p
+    return createElement('article', { classNames: ['card-wrapper'] }, imageWrapper, h2, p);
+}
+
+/**
+ * 
+ * @param {Object} user - об'єкт юзера, картинку для якого створюємо
+ * @returns {HTMLElement} - картинка, яку ми створили
+ */
+function createUserImage(user) {
+    // 1 дія: створюємо картинку
+    const img = document.createElement('img');
+
+    // 2 дія: додаємо атрибути і класи до картинки
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.dataset.id = user.id; // data-id
+    img.classList.add('card-image');
+
+    // 3 дія: реєструємо обробники подій завантаження ресурсу
+    img.addEventListener('load', imageLoadHandler);
+    img.addEventListener('error', imageErrorHandler);
+
+    return img;
+}
+
+function createImageWrapper(user) {
+    // 1 дія: створення обгортки для картинки. Обгортка - placeholder
+    const imgWrapper = createElement('div', { classNames: ['image-wrapper'] });
+    imgWrapper.setAttribute('id', `wrapper${user.id}`);
+    imgWrapper.style.backgroundColor = stringToColor(user.name);
+    // 2 дія: створення картинки
+    const img = createUserImage(user);
+    return imgWrapper;
+}
+
+fetch('./data.json')
+    .then((response) => { return response.json() })
+    .then((data) => {
+        const cardArray = data.map((user) => createCard(user)); // 1
+        root.append(...cardArray); // 2
+    })
+
 
 /*
 
 <article class="card-wrapper">
-            <img src="https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ"
-                alt="John" class="card-image">
+            <div class="image-wrapper">
+                <img src="https://www.ohchr.org/sites/default/files/styles/hero_image_2/public/2021-07/Ethiopia-UN0418425.jpg?itok=7wJB8CbZ"
+                    alt="John" class="card-image">
+            </div>
             <h2 class="username">John</h2>
             <p class="description">user 1</p>
-</article>
+        </article>
 
 */
 
@@ -51,10 +92,23 @@ root.append(...cardArray); // 2
  * @returns {HTMLElement} - створений елемент
  */
 
-function createElement(tagName, {classNames}, ...children) {
+function createElement(tagName, { classNames }, ...children) {
     const elem = document.createElement(tagName);
     elem.classList.add(...classNames);
     elem.append(...children);
 
     return elem;
+}
+
+function stringToColor(str) {
+    let hash = 0;
+    str.split('').forEach(char => {
+        hash = char.charCodeAt(0) + ((hash << 5) - hash)
+    })
+    let colour = '#'
+    for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xff
+        colour += value.toString(16).padStart(2, '0')
+    }
+    return colour
 }
